478d1203ea0784f95b2438724c9d1ba3036cb6bb
user <user@userui-MacBook-Pro.local>
2018-08-01 02:16:18 +0900

    [MOD] 디시인사이드 request시 리퀘스트 헤더 정보 모두 추가

--- helloworld/crawler/BitCoinCrawler.py
+++ helloworld/crawler/BitCoinCrawler.py
@@ -13,9 +13,18 @@ class BitCoinCrawler:
 
     def set_init(self, gall_id, page):
         header = {
-            'Accept': 'text/html,application/xhtml_xml,application/xml;q=0.9,*/*;q=0.8',
+            #'Accept': 'text/html,application/xhtml_xml,application/xml;q=0.9,*/*;q=0.8',
+            #'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:57.0) Gecko/20100101 FireFox/57.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Host': 'gall.dcinside.com',
+            'Referer': 'https://www.google.co.kr/',
+            'Upgrade-Insecure-Requests': '1',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:57.0) Gecko/20100101 FireFox/57.0',
-            'Connection': 'close'
+
         }
         parameter = {'id': gall_id, 'page': page}
         url = self.__url
